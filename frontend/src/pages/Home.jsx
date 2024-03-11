43c85316ec538cb139cdecf553c96bb046d3d7cf
Keerikadan03 <fenseraju@gmail.com>
2024-03-11 15:58:02 +0530

    Revert "error not showing"
    This reverts commit 252a5cb2c17a4f6a5471cbbc282c3c85dfda70d8.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,7 +9,6 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      {/* == hero section == */}
       <section className='hero_section pt-[60px] 2xl:h-[800px]'>
         <div className='container'>
           <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
