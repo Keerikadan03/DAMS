7587ef8311d8670c362e2ae7729f9b68da50ea4f
Keerikadan03 <fenseraju@gmail.com>
2024-03-14 13:42:51 +0530

    added login functionality as well

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,12 @@ import Doctor from '../models/DoctorSchema.js'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 
+const generateToken = user => {
+    return jwt.sign({id: user._id, role: user.role}, process.env.JWT_SECRET_KEY, {
+        expiresIn: "30d",
+    })
+}
+
 export const register = async(req, res) => {
     const { email, password, name, role, photo, gender } = req.body;
     try{
@@ -10,9 +16,9 @@ export const register = async(req, res) => {
 
         //checking if user already exists
         if(role === 'patient'){
-            user = User.findOne({email})
+            user = await User.findOne({email})
         }else if(role === 'doctor'){
-            user = Doctor.findOne({email})
+            user = await Doctor.findOne({email})
         }
 
         if(user){
@@ -57,9 +63,40 @@ export const register = async(req, res) => {
 };
 
 export const login = async(req, res) => {
+    const { email, password } = req.body;
     try{
+        let user = null;
+
+        const patient = User.findOne({email})
+        const doctor = Doctor.findOne({email})
+
+        if(patient){
+            user = patient
+        }
+        if(doctor){
+            user = doctor
+        }
+
+        // is no user found check if user is registered
+
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+
+        //check if entered password is right
+        const isPasswordMatch = await bcrypt.compare(req.body.password, user.password)
+        if(!isPasswordMatch){
+            return res.status(400).json({status: false, message: "Invalid Credentials"})
+        }
+        
+        //generate token
+        const token = generateToken(user);
+
+        const { password, role, appointments, ...rest } = user._doc;
+        return res.status(200).json({status:true, message: 'User logged in Successfully', token, data: {...rest}, role})
 
     }catch(e){
+        return res.status(500).json({status:false, message:'Failed to Login'})
         console.log(e);
     }
 };
