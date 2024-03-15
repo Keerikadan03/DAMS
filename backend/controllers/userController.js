ef00fd6e466a1ef2ae684aac759887d5da9567ee
Keerikadan03 <fenseraju@gmail.com>
2024-03-15 10:44:46 +0530

    added login frontend code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,11 +2,9 @@ import User from "../models/UserSchema";
 
 export const updateUser = async(req,res) => {
     const id = req.params.id
-
     try{
         const updateUser = await User.findByIdAndUpdate(id, {$set: req.body}, {new:true})
         res.status(200).json({success:true, message: 'User successfully updated', data: updateUser});
-
     }catch(e){
         res.status(500).json({success:false, message: 'Failed to update'});
         console.log(e);
@@ -15,11 +13,9 @@ export const updateUser = async(req,res) => {
 
 export const deleteUser = async(req,res) => {
     const id = req.params.id
-
     try{
         const deleteUser = await User.findByIdAndDelete(id)
         res.status(200).json({success:true, message: 'Successfully deleted'});
-
     }catch(e){
         res.status(500).json({success:false, message: 'Failed to delete'});
         console.log(e);
@@ -40,11 +36,9 @@ export const getSingleUser = async(req,res) => {
 }
 
 export const getAllUsers = async(req,res) => {
-
     try{
         const users = await User.findById({})
         res.status(200).json({success:true, message: 'Users found', data: users});
-
     }catch(e){
         res.status(404).json({success:false, message: 'Not found'});
         console.log(e);
