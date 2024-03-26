d5b3077f44a6cc97283c6bc1e3c1594cf0a063dd
Keerikadan03 <fenseraju@gmail.com>
2024-03-26 06:45:27 +0530

    meta.env import not working for some reason so put values in cloudinary file

--- frontend/src/utils/uploadCloudinary.js
+++ frontend/src/utils/uploadCloudinary.js
@@ -1,9 +1,9 @@
-const cloud_name = import.meta.env.VITE_CLOUD_NAME
-const upload_preset = import.meta.env.VITE_UPLOAD_PRESET
+const cloud_name = "keerikadan03"
+const upload_preset = "doctor-appointment-management-system"
 
 const uploadImageToCloudinary = async file => {
-    // console.log(cloud_name)
-    // console.log(upload_preset)
+    console.log(cloud_name)
+    console.log(upload_preset)
     const uploadData = new FormData();
 
     uploadData.append('file', file)
