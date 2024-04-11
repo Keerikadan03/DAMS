f514b194359905b99cd33eeb1b752d5931990dd0
Keerikadan03 <fenseraju@gmail.com>
2024-04-11 11:49:26 +0530

    updated the dummy data to user values

--- frontend/src/Dashboard/user-account/UserDashboard.jsx
+++ frontend/src/Dashboard/user-account/UserDashboard.jsx
@@ -54,13 +54,13 @@ const UserDashboard = () => {
           <div className="pb-[50px] px-[30px] rounded-md">
             <div className="flex items-center justify-center">
             <figure className="w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor">
-              <img src={userimg} alt=""  className="w-full h-full rounded-full"/>
+              <img src={userData.photo} alt=""  className="w-full h-full rounded-full"/>
             </figure>
             </div>
 
             <div className="text-center mt-4">
-              <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">George Shajan</h3>
-              <p className="text-textColor text-[15px] leading-6 font-medium">george@example.com</p>
+              <h3 className="text-[18px] leading-[30px] text-headingColor font-bold">{userData.name}</h3>
+              <p className="text-textColor text-[15px] leading-6 font-medium">{userData.email}</p>
             </div>
 
             <div className="mt-[50px] md:mt-[100px]">
