a6b782118ffba2230ac8a326e145843bc22f1557
Keerikadan03 <fenseraju@gmail.com>
2024-04-11 13:52:31 +0530

    overview card start

--- frontend/src/Dashboard/doctor-account/DoctorDashboard.jsx
+++ frontend/src/Dashboard/doctor-account/DoctorDashboard.jsx
@@ -4,6 +4,7 @@ import Loading from '../../components/Loader/Loading'
 import Error from '../../components/Error/Error'
 import Tabs from './Tabs'
 import { token } from '../../config'
+import starIcon from '../../assets/images/Star.png'
 
 const useGetProfile = (url) => {
 
@@ -49,7 +50,37 @@ const DoctorDashboard = () => {
           !loading && !error &&(
           <div className='grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]'>
             <Tabs tab={tab} setTab={setTab}/>
-            
+            <div className='lg:col-span-2'>
+              <div className='mt-8'>
+                { tab === 'overview' && <div>
+                    <div className='flex items-center gap-4 mb-10'>
+                      <figure className='max-w-[200px] max-h-[200px]'>
+                        <img src={data?.photo} className='w-full' alt="" />
+                      </figure>
+
+                      <div>
+                        <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold'>
+                          Surgeon
+                        </span>
+
+                        <h3 className='text-[22px] leading-9 font-bold text-headingColor mt-3'>George Shajan</h3>
+
+                        <div className='flex items-center gap-[6px]'>
+                          <span className='flex items-center gap-[6px] text-headingColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold'>
+                            <img src={starIcon} alt="" />
+                            4.5
+                          </span>
+                          <span className='text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 font-semibold'>
+                            (233)
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>}
+                { tab === 'appointments' && <div>Appointments</div>}
+                { tab === 'settings' && <div>Profile</div>}
+              </div>
+            </div>
           </div>
         )}
       </div>
