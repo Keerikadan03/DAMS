72cc6f868b2ffa20bb0dfead48df8d27c79975dd
Keerikadan03 <fenseraju@gmail.com>
2024-04-11 11:13:42 +0530

    updated BASE_URL at config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://dams-hw97.onrender.com/api/v1'
+export const BASE_URL = 'https://dams-hw97.onrender.com/api/v1'
 // export const BASE_URL = 'https://dams-eight.vercel.app/api/v1'
 
 export const token = localStorage.getItem('token');
-export const BASE_URL = 'http://localhost:5000/api/v1'
+// export const BASE_URL = 'http://localhost:5000/api/v1'
