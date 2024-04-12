9cce1b16649d9bfa06681132b12409f3b46de5c6
Keerikadan03 <fenseraju@gmail.com>
2024-04-12 08:38:05 +0530

    time slot assignment done

--- frontend/src/Dashboard/doctor-account/Profile.jsx
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { AiOutlineDelete } from 'react-icons/ai'
 
 const Profile = () => {
 
@@ -8,14 +9,60 @@ const Profile = () => {
         phone: '',
         specialization:'',
         ticketPrice: 0,
-        qualifications: [],
-        experiences: [],
-        timeSlots:[]
+        timeSlots:[],
     })
 
-    const handleInputChange = () => {
+    const handleInputChange = (e) => {
         setFormData({...formData, [e.target.name]:e.target.value})
     }
+
+    const updateProfileHandler = async(e) => {
+        e.preventDefault();
+
+    }
+
+    const addItem = (key,item) => {
+        setFormData(prevFormdata => ({...prevFormdata,[key]:[...prevFormdata[key], item]}))
+    }
+
+    const handleChangeFunction = (key, index, event) => {
+        const { name, value } = event.target;
+        setFormData(prevFormData => {
+            const updateItems = [...prevFormData[key]];
+            console.log(updateItems);
+            updateItems[index][name] = value;
+    
+            return {
+                ...prevFormData,
+                [key]: updateItems
+            };
+        });
+    };
+
+    const deleteItem = (key,index) => {
+        setFormData(prevFormData => ({...prevFormData, 
+            [key]:[prevFormData[key].filter((_,i) => i !== index)]}))
+    }
+
+    const handleTimeSlotChange = (event, index) => {
+        handleChangeFunction('timeSlots', index, event);
+    }
+
+    const addTimeSlots = (e) => {
+        e.preventDefault();
+
+        addItem('timeSlots', {
+            day: 'Sunday',
+            startingTime: '10:00',
+            endingTime: '12:00',
+        })
+    }    
+
+    const deleteTimeSlots = (e,index) => {
+        e.preventDefault();
+        deleteItem('timeSlots', index)
+    }
+
   return (
     <div>
         <h2 className='text-headingColor font-bold text-[24px] leading-9 mb-10'>Profile Information</h2>
@@ -83,9 +130,74 @@ const Profile = () => {
                         placeholder='100' 
                         name='ticketPrice'
                         value={formData.ticketPrice}
+                        onChange={handleInputChange}
                         />
                     </div>
                 </div>
+            </div>
+
+            <div className="mb-5">
+                <p className="form_label">Time Slots</p>
+                {
+                    formData.timeSlots?.map((item,index) => (
+                        <div key={index}>
+                            <div>
+                                <div className='grid grid-cols-2 md:grid-cols-4 mb-[30px] gap-5'>
+                                    <div>
+                                        <p className='form_label'>Day</p>
+                                        <select name="day" value={item.day} id=""
+                                        className='form_input py-3.5'
+                                        onChange={(e) => handleTimeSlotChange(e,index)}
+                                        >
+                                            <option value="">Select</option>
+                                            <option value="monday">Monday</option>
+                                            <option value="tuesday">Tuesday</option>
+                                            <option value="wednesday">Wednesday</option>
+                                            <option value="thursday">Thursday</option>
+                                            <option value="friday">Friday</option>
+                                            <option value="saturday">Saturday</option>
+                                            <option value="sunday">Sunday</option>
+                                        </select>
+                                    </div>
+                                    <div>
+                                        <p className='form_label'>Starting Time</p>
+                                        <input type="time" 
+                                        name='startingTime'
+                                        value={item.startingTime}
+                                        className='form_input'
+                                        onChange={e => handleTimeSlotChange(e, index)}
+                                        />
+                                    </div>
+                                    <div>
+                                        <p className='form_label'>Ending Time</p>
+                                        <input type="time" 
+                                        name='endingTime'
+                                        value={item.endingTime}
+                                        className='form_input'
+                                        onChange={e => handleTimeSlotChange(e,index)}
+                                        />
+                                    </div>
+
+                                    <div className='flex items-center'>
+                                    <button onClick={(e) => deleteTimeSlots(e,index)}
+                                     className='bg-red-600 rounded-full p-2 text-white text-[18px] mt-6  cursor-pointer'>
+                                    <AiOutlineDelete />
+                                </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+
+                <button onClick={addTimeSlots}
+                className='bg-[#000] py-2 px-5 rounded text-white h-fit cursor-pointer'>
+                    Add TimeSlot
+                </button>
+            </div>
+
+            <div className='mt-7'>
+                <button type='submit' onClick={updateProfileHandler} className='bg-primaryColor text-white text-[18px] leading-[30px] w-full py-3 px-4 rounded-lg'>Update Profile</button>
             </div>
         </form>
     </div>
