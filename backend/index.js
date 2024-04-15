c81a88f9cc7ed84c6376aeb1964cb12696a25a1c
Keerikadan03 <fenseraju@gmail.com>
2024-04-15 12:00:37 +0530

    allowed cross origins

--- backend/index.js
+++ backend/index.js
@@ -38,8 +38,7 @@ const connectDB = async() => {
 //middleware
     
 app.use(express.json())
-app.use(cors(corsOptions))
-app.use(cors(corsOptions))
+app.use(cors())
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/doctors', doctorRouter);
